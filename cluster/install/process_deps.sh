461ebccda9a1d8f2fb0ce44ffbe88730a3216388
winter.zhang@live.com <winter.zhang@live.com>
2022-12-01 18:13:05 +0800

    fix issues for setting dependency

--- cluster/install/process_deps.sh
+++ cluster/install/process_deps.sh
@@ -4,15 +4,28 @@
 # combined with Common Clause Condition 1.0, as detailed in the NOTICE file.
 
 tries=10
+loops=5
 export LD_LIBRARY_PATH="`pwd`:$LD_LIBRARY_PATH"
-find ../bin -type f | while read f; do
-	cur=0
-	while test "$cur" -lt "$tries"; do
-		ldd $f 2>/dev/null | grep 'not found' >&/dev/null || break
-		ldd $f 2>/dev/null | grep 'not found' |  sed "s#$f:##g" | sed 's#: # #g' | awk '{print $1}' | while read libf; do
-               		cp deps/`basename $libf` .
-        	done
-		let cur++
+tempfile="tmp_out_f"
+loop=0
+while test "$loop" -lt "$loops"; do
+	find ../bin -type f | while read f; do
+		cur=0
+		while test "$cur" -lt "$tries"; do
+			ldd $f 2>/dev/null | grep 'not found' >&/dev/null || break
+			ldd $f 2>/dev/null | grep 'not found' |  sed "s#$f:##g" | sed 's#: # #g' | awk '{print $1}' | while read libf; do
+               			cp deps/`basename $libf` .
+        		done
+			let cur++
+		done
+		while true; do
+			$f --help > $tempfile 2>&1 
+			grep "symbol lookup error" $tempfile >/dev/null 2>/dev/null || break
+			libsopath=`cat $tempfile | sed 's/.*\(symbol lookup error:[^:]*\):.*/\1/g' | cut -d : -f 2 | sed 's/^ *//g' | sed 's/ *$//g'`
+			soname="`basename $libsopath`"
+			test ! -f "deps/$soname" && test -d private && mv -f private private.saved && break
+			cp deps/`basename $libsopath` . || break
+		done
 	done
+	let loop++
 done
-
